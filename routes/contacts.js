fe275f7ca7feacb0882b2ca5dba745934b7bd2bc
unknown <thaivanphu.qn95@gmail.com>
2021-05-17 22:53:58 +0700

    Add Contact Route

--- routes/contacts.js
+++ routes/contacts.js
@@ -24,9 +24,35 @@ router.get('/', auth, async (req, res) => {
 // @route   POST api/contacts
 // @desc   Add new contact
 // @access  Private
-router.post('/', (req, res) => {
-  res.send('Add contact');
-});
+router.post(
+  '/',
+  [auth, [body('name', 'Name is required!').not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, phone, type } = req.body;
+    try {
+      const newContact = new Contact({
+        name,
+        email,
+        phone,
+        type,
+        user: req.user.id,
+      });
+
+      const contact = await newContact.save();
+
+      res.json(contact);
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send('Server Error!');
+    }
+  }
+);
 
 // @route   PUT api/contacts/:id
 // @desc    Update contact
