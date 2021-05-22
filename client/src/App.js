d007b9436d1ed1b1732b16374b45c673c117693b
unknown <thaivanphu.qn95@gmail.com>
2021-05-22 22:48:55 +0700

    PrivateRoute Component

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
 import AlertState from './context/alert/AlertState';
 import setAuthToken from './utils/setAuthToken';
+import PrivateRoute from './components/routing/PrivateRoute';
 
 import './App.css';
 
@@ -29,7 +30,7 @@ const App = () => {
               <div className='container'>
                 <Alerts />
                 <Switch>
-                  <Route exact path='/' component={Home}></Route>
+                  <PrivateRoute exact path='/' component={Home}></PrivateRoute>
                   <Route exact path='/about' component={About}></Route>
                   <Route exact path='/register' component={Register}></Route>
                   <Route exact path='/login' component={Login}></Route>
